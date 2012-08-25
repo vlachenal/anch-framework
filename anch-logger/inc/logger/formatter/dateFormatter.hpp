1bef34a81a90b6fcc665be46d4e21e4e60d6ca8b
Winz <wintsu@gmail.com>
2012-08-25 12:38:09 +0200

    Move from GNU GPL to GNU LGPL

--- anch-logger/inc/logger/formatter/dateFormatter.hpp
+++ anch-logger/inc/logger/formatter/dateFormatter.hpp
@@ -1,18 +1,21 @@
 /*
-    This file is part of ANCH Framework.
+  ANCH Framework: ANother C++ Hack is a C++ framework based on C++11 standart
+  Copyright (C) 2012 Vincent Lachenal
 
-    ANCH Framework is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+  This file is part of ANCH Framework.
 
-    ANCH Framework is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+  ANCH Framework is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with ANCH Framework.  If not, see <http://www.gnu.org/licenses/>.
+  ANCH Framework is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with ANCH Framework.  If not, see <http://www.gnu.org/licenses/>.
 */
 #ifndef _ANCH_LOGGER_FORMATTER_DATE_FORMATTER_H_
 #define _ANCH_LOGGER_FORMATTER_DATE_FORMATTER_H_
--- anch-logger/src/logger/formatter/categoryFormatter.cpp
+++ anch-logger/src/logger/formatter/categoryFormatter.cpp
@@ -1,17 +1,20 @@
 /*
+  ANCH Framework: ANother C++ Hack is a C++ framework based on C++11 standart
+  Copyright (C) 2012 Vincent Lachenal
+
   This file is part of ANCH Framework.
 
   ANCH Framework is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
+  it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
   ANCH Framework is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
+  You should have received a copy of the GNU Lesser General Public License
   along with ANCH Framework.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "logger/formatter/categoryFormatter.hpp"
