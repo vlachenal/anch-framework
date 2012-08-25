e1d7e32135d5e3811a2c99992810bb039116a3ef
Winz <wintsu@gmail.com>
2012-08-25 10:35:08 +0200

    add UDP tests

---
+++ anch-network/test/udpSocketTest.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <thread>
+
+#include "network/tcpSocket.hpp"
+#include "events/observer.hpp"
+
+
+using std::string;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::thread;
+
+using anch::network::TcpSocket;
+using anch::network::SocketEvent;
+using anch::network::IOException;
+using anch::network::Direction;
+
+using anch::events::Observer;
+
+
+class ServerObs : public Observer<SocketEvent> {
+
+public:
+  ServerObs() {};
+  ~ServerObs() {};
+
+public:
+  virtual void notify(const SocketEvent& evt) const throw() {
+    cout << "Server receive " + evt.getMessage() << endl;
+  }
+
+};
+
+class ClientObs : public Observer<SocketEvent> {
+
+public:
+  ClientObs() {};
+  ~ClientObs() {};
+
+public:
+  virtual void notify(const SocketEvent& evt) const throw() {
+    cout << "Client receive " + evt.getMessage() << endl;
+  }
+
+};
+
+
+void
+runUdpServer(UdpSocket* const serverSock) {
+  cout << "Server: Wait for client connection" << endl;
+  // Wait for client
+  UdpSocket csock;
+  serverSock->accept(csock);
+  ServerObs srvObs;
+  csock.addObserver(srvObs);
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  cout << "Server send a message" << endl;
+  try {
+    csock.send("You are connected !!");
+  } catch(const IOException& e) {
+    cerr << "Server " << e.what() << endl;
+  }
+  csock.shutdown(Direction::TRANSMISSION);
+
+  cout << "Server is waiting for a message" << endl;
+  string message;
+  try {
+    csock.receive(message);
+  } catch(const IOException& e) {
+    cerr << "Server " << e.what() << endl;
+  }
+  csock.close();
+}
+
+
+/**
+ * UDP socket unitary tests
+ */
+int
+main(void) {
+  cout << "Enter in UDP sockets unitary test" << endl;
+
+  // Server socket +
+  // Open the server UDP socket for listening on <any>:40099
+  cout << "Create server UDP socket on port 40099" << endl;
+  UdpSocket serverSock("",40099);
+  cout << "bind()" << endl;
+  serverSock.bind();
+  cout << "listen()" << endl;
+  serverSock.listen();
+  thread srvTh(runUdpServer, &serverSock);
+  // Server socket -
+
+  // Client socket +
+  // Open the server UDP socket on localhost:40099
+  cout << "Create client UDP socket on localhost:40099" << endl;
+  UdpSocket clientSocket("127.0.0.1",40099);
+  ClientObs cliObs;
+  clientSocket.addObserver(cliObs);
+  cout << "connect()" << endl;
+  try {
+    clientSocket.connect();
+  } catch(const IOException& e) {
+    cerr << "Client " << e.what() << endl;
+    serverSock.close();
+  }
+  cout << "Client is waiting for message" << endl;
+  string message;
+  clientSocket.receive(message);
+
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  cout << "Client send a message" << endl;
+  clientSocket.send("Bye bye !");
+  clientSocket.shutdown();
+  // Client socket -
+
+  srvTh.join();
+
+  clientSocket.close();
+  serverSock.close();
+
+  cout << "Exit UDP sockets unitary test" << endl;
+  return 0;
+}
