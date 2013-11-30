212f76d00b9bdb75a3464bcc3d9bcb8b962f2ba9
Vincent Lachenal <vincent.lachenal@gmail.com>
2013-11-30 19:06:29 +0100

    Fix project build

---
+++ anch-utils/inc/processor.hpp
@@ -0,0 +1,37 @@
+/*
+  ANCH Framework: ANother C++ Hack is a C++ framework based on C++11 standard
+  Copyright (C) 2012 Vincent Lachenal
+
+  This file is part of ANCH Framework.
+
+  ANCH Framework is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  ANCH Framework is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with ANCH Framework.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef _ANCH_UTILS_PROCESSOR_H_
+#define _ANCH_UTILS_PROCESSOR_H_
+
+#include <limits.h>
+
+#ifndef ANCH64
+#ifndef ANCH32
+
+#if ULONG_MAX == 0xFFFFFFFFFFFFFFFF
+#define ANCH64
+#else
+#define ANCH32
+#endif
+
+#endif // ANCH32
+#endif // ANCH64
+
+#endif // _ANCH_UTILS_PROCESSOR_H_
