548eb75c51acb4acc184a2b8c2e6596d6e26bfa2
Vincent Lachenal <vincent.lachenal@gmail.com>
2017-03-26 11:51:13 +0200

    Documentation

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -6,7 +6,7 @@ var searchData=
   ['_5ffields',['_fields',['../classanch_1_1sql_1_1_result_set.html#a822ebf0a600665b729f95decaba877ac',1,'anch::sql::ResultSet']]],
   ['_5fformatter',['_formatter',['../classanch_1_1logger_1_1_writer.html#a94933843e59000a2f8b7c0ec40b5c1a2',1,'anch::logger::Writer']]],
   ['_5fmessage',['_message',['../classanch_1_1date_1_1formatter_1_1_constant_formatter.html#abbe7e5956c8d5789bc36ab39f23c7101',1,'anch::date::formatter::ConstantFormatter']]],
-  ['_5fnbwildcards',['_nbWildcards',['../classanch_1_1sql_1_1_prepared_statement.html#af309a2c667c4194b8cfe186884afa6e1',1,'anch::sql::PreparedStatement']]],
+  ['_5fnbplaceholders',['_nbPlaceholders',['../classanch_1_1sql_1_1_prepared_statement.html#a44525e33ea1e0f7da07cca8a841843e5',1,'anch::sql::PreparedStatement']]],
   ['_5foutput',['_output',['../classanch_1_1logger_1_1_writer.html#a25e872c774413a214237781ca4c37b4e',1,'anch::logger::Writer']]],
   ['_5fsock',['_sock',['../classanch_1_1network_1_1_socket.html#a0160b9a1e941257eda46129df0b958ae',1,'anch::network::Socket']]],
   ['_5fstmts',['_stmts',['../classanch_1_1sql_1_1_connection.html#ab35f3d23f97a63c1d479bf094d537dcf',1,'anch::sql::Connection']]],
