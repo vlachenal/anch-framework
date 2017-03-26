52978fa4fd59964201b75cd7e153398504c23218
Vincent Lachenal <vincent.lachenal@gmail.com>
2017-03-26 21:45:02 +0200

    Add prepared statement methods to SQL connection class

--- docs/search/functions_12.js
+++ docs/search/functions_12.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['udpsocket',['UdpSocket',['../classanch_1_1network_1_1_udp_socket.html#acd386cff2c60fcd786883c8cfa117df8',1,'anch::network::UdpSocket::UdpSocket()'],['../classanch_1_1network_1_1_udp_socket.html#a81c07697524e2f69eb4c749064523589',1,'anch::network::UdpSocket::UdpSocket(const std::string &amp;ipAddress, uint16_t port)']]],
-  ['update',['update',['../classanch_1_1sql_1_1_connection.html#ab342f819c23a8ad1e763d1bc93f2c7af',1,'anch::sql::Connection']]],
+  ['update',['update',['../classanch_1_1sql_1_1_connection.html#ab342f819c23a8ad1e763d1bc93f2c7af',1,'anch::sql::Connection::update(const std::string &amp;query)'],['../classanch_1_1sql_1_1_connection.html#ad3ec5cab9f60dece303d571fb185a8e5',1,'anch::sql::Connection::update(const std::string &amp;query, const T &amp;value, const Q &amp;... values)']]],
   ['uuid',['Uuid',['../classanch_1_1_uuid.html#af3c19e58d1359bdbaee54f6fb1bc2777',1,'anch::Uuid::Uuid()'],['../classanch_1_1_uuid.html#a351c71105bd793c3a8625574fdeec147',1,'anch::Uuid::Uuid(const Uuid &amp;uuid)']]]
 ];
