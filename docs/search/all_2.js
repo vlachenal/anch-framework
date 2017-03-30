e6205cccbf788422c76fae8391ca0aac30f1a9ec
Vincent Lachenal <vincent.lachenal@gmail.com>
2017-03-30 22:55:51 +0200

    Add batchUpdate method to SQL connection

--- docs/search/all_2.js
+++ docs/search/all_2.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['base64',['Base64',['../classanch_1_1crypto_1_1_base64.html',1,'anch::crypto']]],
+  ['batchupdate',['batchUpdate',['../classanch_1_1sql_1_1_connection.html#a16bb2c8695a30f4aa4832017662aa034',1,'anch::sql::Connection']]],
   ['before',['before',['../classanch_1_1date_1_1_date.html#a568608f4185ccebb97d059f0939fcc73',1,'anch::date::Date']]],
   ['bind',['bind',['../classanch_1_1network_1_1_socket.html#ace1da4f860ac0af46d702cb5fc875a51',1,'anch::network::Socket']]],
   ['blockcipher',['BlockCipher',['../classanch_1_1crypto_1_1_block_cipher.html',1,'anch::crypto']]],
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['batchupdate',['batchUpdate',['../classanch_1_1sql_1_1_connection.html#a16bb2c8695a30f4aa4832017662aa034',1,'anch::sql::Connection']]],
   ['before',['before',['../classanch_1_1date_1_1_date.html#a568608f4185ccebb97d059f0939fcc73',1,'anch::date::Date']]],
   ['bind',['bind',['../classanch_1_1network_1_1_socket.html#ace1da4f860ac0af46d702cb5fc875a51',1,'anch::network::Socket']]],
   ['blockciphermodeofoperation',['BlockCipherModeOfOperation',['../classanch_1_1crypto_1_1_block_cipher_mode_of_operation.html#a782b2bfffec17fe256013dcb0f271f55',1,'anch::crypto::BlockCipherModeOfOperation']]],
