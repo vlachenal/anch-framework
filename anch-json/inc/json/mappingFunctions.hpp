09e675299dd895979c5698e90ac5bd1e147659a5
Vincent Lachenal <vincent.lachenal@gmail.com>
2024-01-23 21:55:03 +0100

    plop ...

--- anch-json/inc/json/mappingFunctions.hpp
+++ anch-json/inc/json/mappingFunctions.hpp
@@ -255,7 +255,7 @@ namespace anch::json {
    */
   template<typename T>
   void deserializeMap(std::istream& input,
-		      std::function<auto(const T&)> pushFunc,
+		      std::function<void(const T&)> pushFunc,
 		      const anch::json::MappingOptions& options,
 		      std::function<void((T& value, std::istream& input, const anch::json::MappingOptions& options))> deserializeFunc);
 
