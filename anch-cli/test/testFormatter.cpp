2008f180acda83045538f174ab7456bc16292c0e
Vincent Lachenal <vincent.lachenal@gmail.com>
2024-09-28 13:03:40 +0200

    Add blink tests

--- anch-cli/test/testFormatter.cpp
+++ anch-cli/test/testFormatter.cpp
@@ -32,11 +32,27 @@ testUnderline() {
 }
 
 void
+testBlink() {
+  std::cout << "Enter in testBlink" << std::endl;
+  std::cout << Formatter::format().blink() << "I am blink text" << std::endl;
+  std::cout << "and it is always blink after new line" << anch::cli::RESET << std::endl;
+  std::cout << "Exit testBlink" << std::endl;
+}
+
+void
 testMix1() {
   std::cout << "Enter in testMix1" << std::endl;
   std::cout << Formatter::format().fgColor(Color::RED).bgColor(Color::BLUE).underline() << "I am foreground text + background text + underline text" << std::endl;
   std::cout << "and it is always formatted after new line" << anch::cli::RESET << std::endl;
   std::cout << "Exit testMix1" << std::endl;
+}
+
+void
+testMix2() {
+  std::cout << "Enter in testMix2" << std::endl;
+  std::cout << Formatter::format().fgColor(Color::RED).bgColor(Color::BLUE).underline().blink() << "I am foreground text + background text + underline text + bink" << std::endl;
+  std::cout << "and it is always formatted after new line" << anch::cli::RESET << std::endl;
+  std::cout << "Exit testMix2" << std::endl;
 }
 
 int
@@ -45,7 +61,9 @@ main(int argc, char** argv) {
     {"fg_blue", testFgBlue},
     {"bg_blue", testBgBlue},
     {"underline", testUnderline},
+    {"blink", testBlink},
     {"mix1", testMix1},
+    {"mix2", testMix2},
   };
   if(argc == 1) { // run all tests
     for(auto iter = tests.cbegin() ; iter != tests.cend() ; ++iter) {
